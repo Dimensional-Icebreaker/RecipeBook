d9f6552cd1ed1ed10239379558f533431c84ad15
Dimensional-Icebreaker <dimensional.icebreaker@gmail.com>
2023-09-23 15:18:36 -0500

    Initial commit

---
+++ app/src/main/java/com/example/recipebook/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.recipebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+
+//import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+//import com.google.firebase.firestore.Query;
+
+public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton addNoteBtn;
+    RecyclerView recyclerView;
+    ImageButton menuBtn;
+    //NoteAdapter noteAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addNoteBtn = findViewById(R.id.add_note_btn);
+        recyclerView = findViewById(R.id.recyler_view);
+        menuBtn = findViewById(R.id.menu_btn);
+
+        addNoteBtn.setOnClickListener((v)-> startActivity(new Intent(MainActivity.this,RecipeDetailsActivity.class)) );
+        menuBtn.setOnClickListener((v)->showMenu() );
+
+    }
+
+    void showMenu(){
+        PopupMenu popupMenu  = new PopupMenu(MainActivity.this,menuBtn);
+        popupMenu.getMenu().add("Logout");
+        popupMenu.show();
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                if(menuItem.getTitle()=="Logout"){
+                    FirebaseAuth.getInstance().signOut();
+                    startActivity(new Intent(MainActivity.this,LoginActivity.class));
+                    finish();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+    }
+}
