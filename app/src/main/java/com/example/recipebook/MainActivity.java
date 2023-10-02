d44f69cb4310c5e9ef39f3ce78d36202d0bd256b
Dimensional-Icebreaker <dimensional.icebreaker@gmail.com>
2023-10-02 15:08:25 -0500

    Commit Number 2 Changes made:- 1.Added API 2.View feature 3.Logout

--- app/src/main/java/com/example/recipebook/MainActivity.java
+++ app/src/main/java/com/example/recipebook/MainActivity.java
@@ -10,42 +10,43 @@ import android.view.MenuItem;
 import android.widget.ImageButton;
 import android.widget.PopupMenu;
 
-//import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
-//import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.Query;
 
 public class MainActivity extends AppCompatActivity {
 
-    FloatingActionButton addNoteBtn;
+    FloatingActionButton addRecipeBtn;
     RecyclerView recyclerView;
     ImageButton menuBtn;
-    //NoteAdapter noteAdapter;
+    RecipeAdapter recipeAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        addNoteBtn = findViewById(R.id.add_note_btn);
+        addRecipeBtn = findViewById(R.id.add_note_btn);
         recyclerView = findViewById(R.id.recyler_view);
         menuBtn = findViewById(R.id.menu_btn);
 
-        addNoteBtn.setOnClickListener((v)-> startActivity(new Intent(MainActivity.this,RecipeDetailsActivity.class)) );
+        addRecipeBtn.setOnClickListener((v)-> startActivity(new Intent(MainActivity.this,RecipeDetailsActivity.class)) );
         menuBtn.setOnClickListener((v)->showMenu() );
+        setupRecyclerView();
 
     }
 
-    void showMenu(){
-        PopupMenu popupMenu  = new PopupMenu(MainActivity.this,menuBtn);
+    void showMenu() {
+        PopupMenu popupMenu = new PopupMenu(MainActivity.this, menuBtn);
         popupMenu.getMenu().add("Logout");
         popupMenu.show();
         popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem menuItem) {
-                if(menuItem.getTitle()=="Logout"){
+                if (menuItem.getTitle() == "Logout") {
                     FirebaseAuth.getInstance().signOut();
-                    startActivity(new Intent(MainActivity.this,LoginActivity.class));
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
                     finish();
                     return true;
                 }
@@ -54,4 +55,32 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    void setupRecyclerView(){
+        Query query  = Utility.getCollectionReferenceForRecipes().orderBy("timestamp",Query.Direction.DESCENDING);
+        FirestoreRecyclerOptions<Recipe> options = new FirestoreRecyclerOptions.Builder<Recipe>()
+                .setQuery(query,Recipe.class).build();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recipeAdapter = new RecipeAdapter(options,this);
+        recyclerView.setAdapter(recipeAdapter);
+    }
+
+        @Override
+        protected void onStart() {
+            super.onStart();
+            recipeAdapter.startListening();
+        }
+
+        @Override
+        protected void onStop() {
+            super.onStop();
+            recipeAdapter.stopListening();
+        }
+
+        @Override
+        protected void onResume() {
+            super.onResume();
+            recipeAdapter.notifyDataSetChanged();
+        }
+
+
 }
